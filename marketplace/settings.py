e141e4b289b75a5a4589b733fd9bb7966abab44e
romanoffnikolaus <romanoffnikolaus@gmail.com>
2023-01-24 10:45:47 +0600

    7

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maursualex@gmail.com'
-EMAIL_HOST_PASSWORD = 'lohbderimgybrrka'
+EMAIL_HOST_USER = 'aursualex@gmail.com'
+EMAIL_HOST_PASSWORD = 'ohbderimgybrrka'
