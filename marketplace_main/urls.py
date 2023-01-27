ff6713adaa56c5f4cccdbe15abfcb22c0d4196be
Александр Маурус <alexmaurus@MacBook-Pro-MSP.local>
2023-01-27 21:34:24 +0600

    added orders, order_history, retrieve_order, fixed some problems

--- marketplace_main/urls.py
+++ marketplace_main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CategoryListView, CommentCreateView, StuffViewSet,CommentCreateView, FavoritesListView, similar_products,CartView
+from .views import CategoryListView, CommentCreateView, StuffViewSet,CommentCreateView, FavoritesListView, similar_products,CartView,OrderView, OrderListView, OrderRetrieveView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -13,6 +13,9 @@ urlpatterns = [
     path('similar/<str:slug>/', similar_products, name='similar_products'),
     path('', include(router.urls)),
     path('favorites/', FavoritesListView.as_view()),
-    path('favorites/<int:pk>/', FavoritesListView.as_view())
+    path('favorites/<int:pk>/', FavoritesListView.as_view()),
+    path('to_order/', OrderView.as_view()),
+    path('order_history/',OrderListView.as_view()),
+    path('order/', OrderRetrieveView.as_view())
 
 ]
